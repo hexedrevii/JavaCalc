15bd6748b4d0ad5f910638e65a42c8da32c1f4a7
HexedRevii <143884194+hexedrevii@users.noreply.github.com>
2025-06-15 21:18:08 +0300

    Fix caret positioning.

--- src/main/java/xyz/itseve/javacalc/CalculatorController.java
+++ src/main/java/xyz/itseve/javacalc/CalculatorController.java
@@ -2,18 +2,32 @@ package xyz.itseve.javacalc;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import xyz.itseve.shuntingyard.ArithmeticExpression;
 
-public class CalculatorController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalculatorController implements Initializable {
     private boolean nextDelete = false;
+    private int caret = 0;
 
     @FXML private TextField output;
 
     @FXML private Button equalsButton;
     public Button getEqualsButton() {
         return equalsButton;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        output.caretPositionProperty().addListener(
+            (obs, oldPos, newPos) -> {
+                caret = newPos.intValue();
+            }
+        );
     }
 
     @FXML private void inputChanged() {
@@ -30,7 +44,7 @@ public class CalculatorController {
         }
 
         Button self = (Button)event.getSource();
-        output.appendText(self.getText());
+        output.insertText(caret, self.getText());
     }
 
     @FXML private void equalClick() {
@@ -57,6 +71,8 @@ public class CalculatorController {
         if (!output.getText().isEmpty()) {
             String cut = (text.substring(0, text.length() - 1));
             output.setText(cut);
+
+            output.positionCaret(output.getText().length());
         }
     }
 
